01256712cb4e484e3f9fc97f91348af8e2d74f47
Mike <mjeanjacques90@gmail.com>
2019-04-18 00:41:22 -0400

    1st commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+    var targetScore = Math.floor(Math.random() * 102) + 19;
+    runningScore = 0;
+    gameWin = 0;
+    gamelose = 0;
+
+    
+    var crystalObj = {
+    one : Math.floor(Math.random() * 12) + 1,
+    two : Math.floor(Math.random() * 12) + 1,
+    three : Math.floor(Math.random() * 12) + 1,
+    four : Math.floor(Math.random() * 12) + 1};
+    
+    function resetGame(){
+        var crystalObj = {
+            one : Math.floor(Math.random() * 12) + 1,
+            two : Math.floor(Math.random() * 12) + 1,
+            three : Math.floor(Math.random() * 12) + 1,
+            four : Math.floor(Math.random() * 12) + 1};
+
+        var targetScore = Math.floor(Math.random() * 102) + 19;
+        runningScore = 0;
+        $('#targetScore').text(targetScore);
+        $('#currentScore').text(runningScore);
+    }
+
+    function game(){
+        if (targetScore == runningScore){
+            gameWin++;
+            $('#win').text(gameWin);
+            resetGame();
+        }
+    
+        if (runningScore > targetScore){
+            gamelose++;
+            $('#lose').text(gamelose);
+            resetGame();
+        }
+    }
+
+
+    console.log(crystalObj.two);
+
+    
+    $('#targetScore').text(targetScore);
+
+    $('#crystal1').on('click', function(event){
+        runningScore = runningScore + crystalObj.one;
+        $('#currentScore').text(runningScore);
+        game();
+    })
+    $('#crystal2').on('click', function(event){
+        runningScore = runningScore + crystalObj.two;
+        $('#currentScore').text(runningScore);
+        game();
+    })
+    $('#crystal3').on('click', function(event){
+        runningScore = runningScore + crystalObj.three;
+        $('#currentScore').text(runningScore);
+        game();
+    })
+    $('#crystal4').on('click', function(event){
+        runningScore = runningScore + crystalObj.four;
+        $('#currentScore').text(runningScore);
+        game();
+    })
+
+
+});
